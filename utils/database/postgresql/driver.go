cb32a15f33fff36e7c34421ed55d5ce4b2de22e0
yusnarsetiyadi <yusnarsetiyadi150403@gmail.com>
2023-01-29 10:39:02 +0700

    first set up middleware and connecton database postgresql/mysql

---
+++ utils/database/postgresql/driver.go
@@ -0,0 +1,30 @@
+package postgresql
+
+import (
+	"fmt"
+	"log"
+	"myproject/innonformaledu/config"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func initDB(cfg *config.AppConfig) *gorm.DB {
+	postgresqlDB := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Jakarta", cfg.DB_HOST, cfg.DB_USERNAME, cfg.DB_PASSWORD, cfg.DB_NAME, cfg.DB_PORT)
+	db, err := gorm.Open(postgres.Open(postgresqlDB), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Cannot connect to DB")
+	}
+
+	// migrateDB(db)
+
+	return db
+}
+
+// func migrateDB(db *gorm.DB){
+
+// }
