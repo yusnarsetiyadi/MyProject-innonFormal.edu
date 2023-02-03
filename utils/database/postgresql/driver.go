eac34ddad8b0856df24a11216f200c0c1433d14f
yusnarsetiyadi <yusnarsetiyadi150403@gmail.com>
2023-02-03 17:21:33 +0700

    prepare local db postgres and mysql

--- utils/database/postgresql/driver.go
+++ utils/database/postgresql/driver.go
@@ -10,11 +10,13 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-func initDB(cfg *config.AppConfig) *gorm.DB {
+func InitPostgreSQLDB(cfg *config.AppConfig) *gorm.DB {
 	postgresqlDB := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Jakarta", cfg.DB_HOST, cfg.DB_USERNAME, cfg.DB_PASSWORD, cfg.DB_NAME, cfg.DB_PORT)
 	db, err := gorm.Open(postgres.Open(postgresqlDB), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
+
+	fmt.Print("connection postgres success")
 
 	if err != nil {
 		log.Fatal("Cannot connect to DB")
