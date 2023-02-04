d61c6ab093c58905f9d82ac24eaa38efa197ea5b
yusnarsetiyadi <yusnarsetiyadi150403@gmail.com>
2023-02-04 14:25:29 +0700

    add crud feedback

--- features/feedback/delivery/handler.go
+++ features/feedback/delivery/handler.go
@@ -1,1 +1,115 @@
 package delivery
+
+import (
+	"myproject/innonformaledu/features/feedback"
+	"myproject/innonformaledu/middlewares"
+	"myproject/innonformaledu/utils/helper"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+type FeedbackDelivery struct {
+	feedbackService feedback.ServiceInterface
+}
+
+func New(service feedback.ServiceInterface, e *echo.Echo) {
+	handler := &FeedbackDelivery{
+		feedbackService: service,
+	}
+
+	e.GET("/feedbacks", handler.GetAll)
+	e.GET("/feedbacks/:id", handler.GetById)
+	e.POST("/feedbacks", handler.Create)
+	e.PUT("/feedbacks/:id", handler.Update, middlewares.JWTMiddleware())
+	e.DELETE("/feedbacks/:id", handler.Delete, middlewares.JWTMiddleware())
+}
+
+func (delivery *FeedbackDelivery) GetAll(c echo.Context) error {
+	results, err := delivery.feedbackService.GetAll()
+	if err != nil {
+		if strings.Contains(err.Error(), "Get data success. No data.") {
+			return c.JSON(http.StatusOK, helper.SuccessWithDataResponse(err.Error(), results))
+		}
+		return c.JSON(http.StatusBadRequest, helper.FailedResponse(err.Error()))
+	}
+
+	dataResponse := fromCoreList(results)
+
+	return c.JSON(http.StatusOK, helper.SuccessWithDataResponse("Success read all data.", dataResponse))
+}
+
+func (delivery *FeedbackDelivery) GetById(c echo.Context) error {
+	idParam := c.Param("id")
+	id, errConv := strconv.Atoi(idParam)
+	if errConv != nil {
+		return c.JSON(http.StatusBadRequest, helper.FailedResponse("Error. Id must integer."))
+	}
+	results, err := delivery.feedbackService.GetById(uint(id))
+	if err != nil {
+		if strings.Contains(err.Error(), "Get data success. No data.") {
+			return c.JSON(http.StatusOK, helper.SuccessWithDataResponse(err.Error(), results))
+		}
+		return c.JSON(http.StatusBadRequest, helper.FailedResponse(err.Error()))
+	}
+
+	dataResponse := fromCore(results)
+
+	return c.JSON(http.StatusOK, helper.SuccessWithDataResponse("Success read data.", dataResponse))
+}
+
+func (delivery *FeedbackDelivery) Create(c echo.Context) error {
+	input := FeedbackRequest{}
+	errBind := c.Bind(&input)
+	if errBind != nil {
+		return c.JSON(http.StatusBadRequest, helper.FailedResponse("Error binding data. "+errBind.Error()))
+	}
+	dataCore := toCore(input)
+	err := delivery.feedbackService.Create(dataCore)
+	if err != nil {
+		if strings.Contains(err.Error(), "Error:Field validation") {
+			return c.JSON(http.StatusBadRequest, helper.FailedResponse("Some field cannot Empty. Details : "+err.Error()))
+		}
+		return c.JSON(http.StatusInternalServerError, helper.FailedResponse("Failed insert data. "+err.Error()))
+	}
+	return c.JSON(http.StatusCreated, helper.SuccessResponse("Success create data"))
+}
+
+func (delivery *FeedbackDelivery) Update(c echo.Context) error {
+	idParam := c.Param("id")
+	id, errConv := strconv.Atoi(idParam)
+	if errConv != nil {
+		return c.JSON(http.StatusBadRequest, helper.FailedResponse("Error. Id must integer."))
+	}
+	input := FeedbackRequest{}
+	errBind := c.Bind(&input)
+	if errBind != nil {
+		return c.JSON(http.StatusBadRequest, helper.FailedResponse("Error binding data. "+errBind.Error()))
+	}
+	dataCore := toCore(input)
+	err := delivery.feedbackService.Update(dataCore, uint(id))
+	if err != nil {
+		if strings.Contains(err.Error(), "Error:Field validation") {
+			return c.JSON(http.StatusBadRequest, helper.FailedResponse("Some field cannot Empty. Details : "+err.Error()))
+		}
+		return c.JSON(http.StatusInternalServerError, helper.FailedResponse("Failed update data. "+err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, helper.SuccessResponse("Success update data."))
+}
+
+func (delivery *FeedbackDelivery) Delete(c echo.Context) error {
+	idParam := c.Param("id")
+	id, errConv := strconv.Atoi(idParam)
+	if errConv != nil {
+		return c.JSON(http.StatusBadRequest, helper.FailedResponse("Error. Id must integer."))
+	}
+	err := delivery.feedbackService.Delete(uint(id))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, helper.FailedResponse(err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, helper.SuccessResponse("Success delete data."))
+}
--- features/feedback/delivery/request.go
+++ features/feedback/delivery/request.go
@@ -1,1 +1,20 @@
 package delivery
+
+import "myproject/innonformaledu/features/feedback"
+
+type FeedbackRequest struct {
+	Rating    float64 `json:"rating" form:"rating"`
+	Feedback  string  `json:"feedback" form:"feedback"`
+	StudentID uint    `json:"student_id" form:"student_id"`
+	ClassID   uint    `json:"class_id" form:"class_id"`
+}
+
+func toCore(input FeedbackRequest) feedback.FeedbackCore {
+	dataCore := feedback.FeedbackCore{
+		Rating:    input.Rating,
+		Feedback:  input.Feedback,
+		StudentID: input.StudentID,
+		ClassID:   input.ClassID,
+	}
+	return dataCore
+}
